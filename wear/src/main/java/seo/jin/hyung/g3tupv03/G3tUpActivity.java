0c1b49e7c7553fa89b5a15dfd701d55239681f76
cailot <cailot@telstra.com>
2015-07-19 23:01:38 +1000

    UI complete with image

--- wear/src/main/java/seo/jin/hyung/g3tupv03/G3tUpActivity.java
+++ wear/src/main/java/seo/jin/hyung/g3tupv03/G3tUpActivity.java
@@ -53,21 +53,21 @@ import seo.jin.hyung.g3tupv03.utils.G3tUpConstants;
  * FLAG_KEEP_SCREEN_ON to give user adequate time for taking actions but since we don't want to
  * keep screen on for an extended period of time, there is a SCREEN_ON_TIMEOUT_MS that is enforced
  * if no interaction is discovered.
- *
+ * <p>
  * This activity includes a {@link android.support.v4.view.ViewPager} with two pages, one that
  * shows the current count and one that allows user to reset the counter. the current value of the
  * counter is persisted so that upon re-launch, the counter picks up from the last value. At any
  * stage, user can set this counter to 0.
- *
+ * <p>
  * What to do ?
- *  1. start timer
- *  2. when timer reaches to 0
- *      2-1 trigger alarm & vibration on phone
- *      2-2 start animation
- *  3. update exercise count
- *  4. when count reaches to goal
- *      4-1 trigger stop alarm & vibration on phone
- *      4-2 display good message
+ * 1. start timer
+ * 2. when timer reaches to 0
+ * 2-1 trigger alarm & vibration on phone
+ * 2-2 start animation
+ * 3. update exercise count
+ * 4. when count reaches to goal
+ * 4-1 trigger stop alarm & vibration on phone
+ * 4-2 display good message
  */
 public class G3tUpActivity extends Activity
         implements SensorEventListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
@@ -76,7 +76,6 @@ public class G3tUpActivity extends Activity
     private Sensor sensor;
     private long lastTime = 0;
     private boolean up;
-
 
 
     private int jumpCounter = 0;
@@ -92,7 +91,7 @@ public class G3tUpActivity extends Activity
     private long timerDuration;
     private int exerciseCount;
 
-    private String version = "v09";
+    private String version = "v11";
 
 
     @Override
@@ -107,45 +106,56 @@ public class G3tUpActivity extends Activity
         selectFragment();
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-//        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-//        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-
     }
 
 
     // initialise component, in this case it just show Button for test
-    private void setUpViews()
-    {
+    private void setUpViews() {
 
         btnStop = (Button) findViewById(R.id.btnStop);
+        btnStop.setVisibility(View.INVISIBLE);
     }
 
     // load info from SharedPreference
-    private void setUpThreshold()
-    {
+    private void setUpThreshold() {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+
         timerDuration = Long.parseLong(preferences.getString("timerSet", "30"));
-        exerciseCount = Integer.parseInt(preferences.getString("exerciseSet","5"));
+
+
+        exerciseCount = Integer.parseInt(preferences.getString("exerciseSet", "5"));
         btnStop.setText(exerciseCount + " - " + version);
     }
 
 
     // determine displaying fragment based on status
-    private void selectFragment()
-    {
+    private void selectFragment() {
         fragment = null;
         // If CounterFragment is about to display, trigger timer as well
-        if(status==G3tUpConstants.COUNTER_STATE)
-        {
+        if (status == G3tUpConstants.COUNTER_STATE) {
             fragment = new CounterFragment();
-            CountDownTimer timer = new MyCountDownTimer(G3tUpConstants.SECOND*timerDuration, G3tUpConstants.SECOND);
+            CountDownTimer timer = new MyCountDownTimer(G3tUpConstants.SECOND * timerDuration, G3tUpConstants.SECOND);
             timer.start();
-        // ExerciseFragment
-        }else if(status==G3tUpConstants.EXERCISE_STATE){
+
+            // ExerciseFragment
+        } else if (status == G3tUpConstants.EXERCISE_STATE) {
+
+            ///////////////////////
+            // Temporary Button
+            //////////////////////
+            btnStop.setVisibility(View.VISIBLE);
+
             fragment = new ExerciseFragment();
 
-        // DisplayFragment
-        }else{
+            // DisplayFragment
+        } else {
+
+            ///////////////////////
+            // Temporary Button
+            //////////////////////
+            btnStop.setVisibility(View.INVISIBLE);
+
             fragment = new DisplayFragment();
         }
         FragmentManager fm = getFragmentManager();
@@ -155,20 +165,18 @@ public class G3tUpActivity extends Activity
     }
 
     // event handler when button pressed. this is just for testing purpose
-    public void clickButton(View view)
-    {
-        if(status==G3tUpConstants.COUNTER_STATE) {
+    public void clickButton(View view) {
+        if (status == G3tUpConstants.COUNTER_STATE) {
 
-        }else if(status == G3tUpConstants.EXERCISE_STATE) {
+        } else if (status == G3tUpConstants.EXERCISE_STATE) {
             increaseCount();
-        }else{
+        } else {
             // dismiss
             Log.e(G3tUpConstants.TAG, "Dismiss Activity ?");
         }
     }
 
-    private void increaseCount()
-    {
+    private void increaseCount() {
         jumpCounter++;
         Log.d(G3tUpConstants.TAG, "Exercise - " + jumpCounter);
 
@@ -186,25 +194,13 @@ public class G3tUpActivity extends Activity
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     @Override
     protected void onResume() {
         super.onResume();
+
+        // update threshold
+        setUpThreshold();
+
         if (sensorManager.registerListener(this, sensor,
                 SensorManager.SENSOR_DELAY_NORMAL)) {
             if (Log.isLoggable(G3tUpConstants.TAG, Log.DEBUG)) {
@@ -242,7 +238,7 @@ public class G3tUpActivity extends Activity
      */
     private void detectJump(float xValue, long timestamp) {
         if ((Math.abs(xValue) > G3tUpConstants.GRAVITY_THRESHOLD)) {
-            if(timestamp - lastTime < G3tUpConstants.TIME_THRESHOLD_NS && up != (xValue > 0)) {
+            if (timestamp - lastTime < G3tUpConstants.TIME_THRESHOLD_NS && up != (xValue > 0)) {
                 onJumpDetected(!up);
             }
             up = xValue > 0;
@@ -263,8 +259,7 @@ public class G3tUpActivity extends Activity
     }
 
     // This is timer class for CounterFragment
-    public class MyCountDownTimer extends CountDownTimer
-    {
+    public class MyCountDownTimer extends CountDownTimer {
         public MyCountDownTimer(long millisInFuture, long countDownInterval) {
             super(millisInFuture, countDownInterval);
         }
@@ -272,7 +267,7 @@ public class G3tUpActivity extends Activity
         @Override
         public void onTick(long millisUntilFinished) {
 //            Log.e(G3tUpConstants.TAG, "" + millisUntilFinished/1000);
-            long seconds = (long) millisUntilFinished/1000;
+            long seconds = (long) millisUntilFinished / 1000;
             String remain = String.format("%d Min %d Sec",
                     TimeUnit.SECONDS.toMinutes(seconds),
                     TimeUnit.SECONDS.toSeconds(seconds) -
@@ -298,16 +293,12 @@ public class G3tUpActivity extends Activity
     }
 
 
-
-
-
     ///////////////////////////////////////////////////////////////////////////////
 
     GoogleApiClient googleApiClient;
     String message;
 
-    private void prepareCommunication()
-    {
+    private void prepareCommunication() {
         GoogleApiClient.Builder builder = new GoogleApiClient.Builder(this);
         builder.addApi(Wearable.API);
         builder.addConnectionCallbacks(this);
@@ -321,25 +312,21 @@ public class G3tUpActivity extends Activity
 //        triggerActionOnPhone();
     }
 
-    public void triggerActionOnPhone()
-    {
-        if(googleApiClient!=null && googleApiClient.isConnected())
-        {
+    public void triggerActionOnPhone() {
+        if (googleApiClient != null && googleApiClient.isConnected()) {
             new SendMessageToDataLayer(G3tUpConstants.COMMUNICATIO_PATH, message).start();
             Log.e(G3tUpConstants.TAG, "Communication thread has triggered");
-        }else{
+        } else {
             Log.e(G3tUpConstants.TAG, "Failed to send messages");
         }
     }
 
 
-    public class SendMessageToDataLayer extends Thread
-    {
+    public class SendMessageToDataLayer extends Thread {
         String path;
         String message;
 
-        public SendMessageToDataLayer(String path, String message)
-        {
+        public SendMessageToDataLayer(String path, String message) {
             this.path = path;
             this.message = message;
         }
@@ -347,26 +334,18 @@ public class G3tUpActivity extends Activity
         @Override
         public void run() {
             NodeApi.GetConnectedNodesResult nodesResult = Wearable.NodeApi.getConnectedNodes(googleApiClient).await();
-            for(Node node : nodesResult.getNodes())
-            {
+            for (Node node : nodesResult.getNodes()) {
                 MessageApi.SendMessageResult messageResult = Wearable.MessageApi.sendMessage(googleApiClient, node.getId(), path, message.getBytes()).await();
-                if(messageResult.getStatus().isSuccess())
-                {
+                if (messageResult.getStatus().isSuccess()) {
                     Log.e(G3tUpConstants.TAG, "Sent to " + node.getDisplayName());
                     Log.e(G3tUpConstants.TAG, "Node Id is " + node.getId());
                     Log.e(G3tUpConstants.TAG, "Node size is " + nodesResult.getNodes().size());
-                }else{
+                } else {
                     Log.e(G3tUpConstants.TAG, "Error while sending message");
                 }
             }
         }
     }
-
-
-
-
-
-
 
 
     @Override
@@ -388,8 +367,7 @@ public class G3tUpActivity extends Activity
 
     @Override
     protected void onStop() {
-        if(googleApiClient!=null && googleApiClient.isConnected())
-        {
+        if (googleApiClient != null && googleApiClient.isConnected()) {
             googleApiClient.disconnect();
         }
         super.onStop();
